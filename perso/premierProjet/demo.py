56043617fd3e42cf6746bc4bf76a909b3322f09e
Tentaichi <benjamin.colas.pro@gmail.com>
2023-05-30 12:31:48 +0200

    Create demo.py

---
+++ perso/premierProjet/demo.py
@@ -0,0 +1,12 @@
+#print("Salut") #Affiche une donnée de type chaine de caractère
+#print(2023) #Affiche une donnée de type nombre
+#print(" ============ ")
+#print(" < Mon code > ") #test
+#print(" ============ ")
+
+leNom = input("Veuillez saisir votre prénom : ") #Permet de saisir une donnée dans une variable
+#print("Votre nom : " + leNom) #Affiche le nom concaténé avec une chaine de caractère
+
+anneeNaissance = int(input("Veuillez saisir votre date de naissance : "))
+age = 2023 - anneeNaissance
+print(leNom, "né en", anneeNaissance, "as donc", age, "ans.")
