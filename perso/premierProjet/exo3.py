93cdea68eb78f0c9e682fb8197ae6be88adbcbc7
Tentaichi <benjamin.colas.pro@gmail.com>
2023-06-01 17:21:46 +0200

    soup fonctionnel

--- perso/premierProjet/exo3.py
+++ perso/premierProjet/exo3.py
@@ -1,12 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
-import re
+import json
 
-req = requests.get("https://www.frameip.com/liste-des-ports-tcp-udp/?plage=1")
-tabl = BeautifulSoup(req.content, 'html.parser')
-# title = soup.find("h1")
-for aa in tabl.find_all('tr'):
-    ff = aa.find("td")
-    gg = ff.find("p").string
-    print(gg)
-    break
+allPorts = {}
+i = 1
+while i < 10:
+    req = requests.get(f"https://www.frameip.com/liste-des-ports-tcp-udp/?plage={i}")
+    soup = BeautifulSoup(req.content, 'html.parser').find("table")
+    soupRows = soup.find_all('tr')
+
+    for soupRow in soupRows[1:]:
+        port = soupRow.find("a").string
+        protocol = soupRow.find_all("a")[1].string
+        name = soupRow.find("p").string.strip()
+        desc = soupRow.find_all("p")[3].string.strip()
+        allPorts[port] = {}
+        allPorts[port][protocol] = {}
+        allPorts[port][protocol][name] = desc
+    i += 1
+
+with open("ports.json", "w") as json_file_pointer:
+  json.dump(allPorts, json_file_pointer, indent=4)
+
+print(allPorts)
